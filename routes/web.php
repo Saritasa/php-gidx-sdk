bf122360ab51e91cee283eda72771303522c267a
Sergey Populov <sergey@saritasa.com>
2021-01-15 18:39:08 +0500

    Upload document action

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::post('webhook', 'GidxController@handleWebhook')->name('webhook');
+Route::post('documents', 'GidxController@uploadDocument')->name('documents');
--- src/Http/Controllers/GidxController.php
+++ src/Http/Controllers/GidxController.php
@@ -2,11 +2,17 @@
 
 namespace GidxSDK\Http\Controllers;
 
+use GidxSDK\Enums\GidxDocumentParams;
+use GidxSDK\Enums\GidxDocumentStatuses;
+use GidxSDK\Enums\GidxParams;
 use GidxSDK\Events\GidxWebhookReceived;
 use GidxSDK\Http\Requests\Request;
+use GidxSDK\Http\Requests\UploadDocumentRequest;
+use GidxSDK\Services\GidxClient;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class GidxController extends Controller
 {
@@ -17,6 +23,29 @@ class GidxController extends Controller
         Log::debug("Gidx callback from ip: " . $request->ip(), $payload);
         event(new GidxWebhookReceived($payload));
 
+        return new Response('', Response::HTTP_NO_CONTENT);
+    }
+
+    public function uploadDocument(UploadDocumentRequest $request, GidxClient $gidxClient)
+    {
+        $documentFile = $request->file('file');
+        $user = $request->user();
+
+        $params = [
+            'file' => curl_file_create(
+                $documentFile->getRealPath(),
+                $documentFile->getMimeType(),
+                $documentFile->getClientOriginalName()
+            ),
+            'json' => [
+                GidxParams::MERCHANT_SESSION_ID => Str::uuid()->toString(),
+                GidxParams::MERCHANT_CUSTOMER_ID => $user->getOrCreateGidxId(),
+                GidxDocumentParams::CATEGORY_TYPE => (int)$request->category_type,
+                GidxDocumentParams::DOCUMENT_STATUS => GidxDocumentStatuses::NOT_REVIEWED,
+            ],
+        ];
+
+        $gidxClient->uploadDocument($params);
         return new Response('', Response::HTTP_NO_CONTENT);
     }
 }
